e7eb514f0a6b86d97be7b972dc7fc1d2450751f1
Francesco Bergonzini <fbergonzinigames@gmail.com>
2021-11-27 12:09:48 +0100

    Scena UI con scanner + carosello da aggiungere

---
+++ ASAC_UNITY_ARAP/Assets/ASAC_AR/Francesco/Script/IntroCanvasController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+
+public class IntroCanvasController : MonoBehaviour
+{
+    //
+    ARTrackedImageManager _trackedManager;
+    //
+    [SerializeField] Animator scanAnimat;
+    //
+    [SerializeField] Text debugtext;
+
+    private void Awake()
+    {
+        _trackedManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    #region subscrive event
+    private void OnEnable()
+    {
+        //rileva l'evento di intercettamento dell'immagine
+        _trackedManager.trackedImagesChanged += ChangePrefabOnImage;
+    }
+
+
+    private void OnDisable()
+    {
+        _trackedManager.trackedImagesChanged -= ChangePrefabOnImage;
+    }
+
+    #endregion
+
+    private void ChangePrefabOnImage(ARTrackedImagesChangedEventArgs images)
+    {
+        foreach (ARTrackedImage image in images.added)
+        {
+            if (image.referenceImage.name == "QR_Code")
+            {
+                //attiva animazione scan
+                scanAnimat.SetTrigger("Scan");
+                debugtext.text = "Animazione in corso";
+            }
+        }
+
+    }
+}
