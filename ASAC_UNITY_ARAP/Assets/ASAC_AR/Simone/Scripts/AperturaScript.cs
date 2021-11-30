b97f6aaf0ba31a1df21a1c4e744ff6f78c0634ac
Francesco Bergonzini <fbergonzinigames@gmail.com>
2021-11-30 17:28:17 +0100

    Prefab faldone > scena Simo
    Simo > animazione faldone
    Bergo > look at scatola nera

--- ASAC_UNITY_ARAP/Assets/ASAC_AR/Simone/Scripts/AperturaScript.cs
+++ ASAC_UNITY_ARAP/Assets/ASAC_AR/Simone/Scripts/AperturaScript.cs
@@ -6,16 +6,6 @@ public class AperturaScript : MonoBehaviour
 {
     [SerializeField] GameObject faldoni;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void StartFaldoniAnim()
     {
